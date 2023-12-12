50d75ce43f9497aafb7f09b14147120a56b444c2
Dani lukman <danilukman2206@gmail.com>
2023-12-13 00:11:06 +0700

    asd

---
+++ app/src/main/java/com/millenialzdev/logindanregistervolleymysql/Db_Contract.java
@@ -0,0 +1,10 @@
+package com.millenialzdev.logindanregistervolleymysql;
+
+public class Db_Contract {
+
+    public static String ip = "http://192.168.0.113:3000";
+
+    public static final String urlRegister = ""+ip+"/api/signup";
+    public static final String urlLogin = ""+ip+"/api/login";
+
+}
